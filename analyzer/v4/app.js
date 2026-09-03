416f0b3f57ac9fa98e6753b8f94452fb828457e9
Srivasu215 <sri.vasu215@gmail.com>
2026-09-03 17:54:45 +0530

    V4 started 5 files added

---
+++ analyzer/v4/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import { router as routerFromApi } from './Api/routes.js';
+
+const app = express()
+
+const port =  3000;
+
+app.use("/Api", routerFromApi);
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
